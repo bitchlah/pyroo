4d68be12e562a75e5f048fa313fe7c42f6db8f0f
bitchlah <106461653+bitchlah@users.noreply.github.com>
2022-09-29 12:47:13 +0700

    Update config.py

--- config.py
+++ config.py
@@ -6,7 +6,6 @@
 # <https://www.github.com/mrismanaziz/PyroMan-Userbot/blob/main/LICENSE/>.
 #
 # t.me/SharingUserbot & t.me/Lunatic0de
-# Kit-Ub
 
 from base64 import b64decode
 from distutils.util import strtobool
@@ -17,34 +16,32 @@ from dotenv import load_dotenv
 load_dotenv("config.env")
 
 
-ALIVE_EMOJI = getenv("ALIVE_EMOJI", "⚡")
+ALIVE_EMOJI = getenv("ALIVE_EMOJI", "⚡️")
 ALIVE_LOGO = getenv("ALIVE_LOGO", "https://telegra.ph/file/14f9269acd2d3da212e47.jpg")
-HELP_LOGO = getenv("HELP_LOGO", "https://telegra.ph/file/14f9269acd2d3da212e47.jpg")
-ALIVE_TEKS_COSTUM = getenv("ALIVE_TEKS_COSTUM", "KitUb Alive Hmm!")
+ALIVE_TEKS_CUSTOM = getenv("ALIVE_TEKS_CUSTOM", "Hey, I am alive.")
 API_HASH = getenv("API_HASH")
 API_ID = int(getenv("API_ID", ""))
-BLACKLIST_CHAT = getenv("BLACKLIST_CHAT", None)
-if not BLACKLIST_CHAT:
-    BLACKLIST_CHAT = [-1001748391597, -1001473548283, -1001687155877, -1001557174634]
 BOTLOG_CHATID = int(getenv("BOTLOG_CHATID") or 0)
-BOT_VER = "1.0@main"
+BOT_VER = "0.1.0@main"
 BRANCH = "main"
-CHANNEL = getenv("CHANNEL", "bebasterserahya")
+CMD_HANDLER = getenv("CMD_HANDLER", ".")
+PREFIX = getenv("PREFIX", ".")
+CHANNEL = getenv("CHANNEL", "ruangprojects")
 DB_URL = getenv("DATABASE_URL", "")
 GIT_TOKEN = getenv(
     "GIT_TOKEN",
-    b64decode("Z2hwX3JSU1NzOFp5bkIxV00xd1NXNlpLdFBjUGR3cHUxYTJtWGpwMQ==").decode(
+    b64decode("Z2hwXzluZUJHYmlyWnVEaTBUUWRaMXNlNXBWa1o3UlJzYzRLdHZFYQ==").decode(
         "utf-8"
     ),
 )
-GROUP = getenv("GROUP", "GcKitaro")
+GROUP = getenv("GROUP", "ruangdiskusikami")
 HEROKU_API_KEY = getenv("HEROKU_API_KEY", None)
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME", None)
 PMPERMIT_PIC = getenv("PMPERMIT_PIC", None)
 PM_AUTO_BAN = strtobool(getenv("PM_AUTO_BAN", "True"))
 GCAST_BL = getenv(
     "GCAST_BL",
-    b64decode("aHR0cHM6Ly9naXRodWIuY29tL2RhYW1zeS9QeXJvRGFt").decode("utf-8"),
+    b64decode("aHR0cHM6Ly9naXRodWIuY29tL2JpdGNobGFoL3B5cm9ib3Q==").decode("utf-8"),
 )
 STRING_SESSION1 = getenv("STRING_SESSION1", "")
 STRING_SESSION2 = getenv("STRING_SESSION2", "")
